e9990bdf13dc3d994196b982aa7ed3ab1bf9f4fd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-26 13:34:32 +0200

    adapt log_token for integers

--- src/log/log.c
+++ src/log/log.c
@@ -4,9 +4,10 @@
 
 void log_token(t_token token)
 {
-    printf("%s", token_repr(token));
-    if (token.literal)
-        printf("\t%s", token.literal);
+    if (token.type == INTEGER)
+        printf("%i", token.value);
+    else
+        printf("%s", token_repr(token));
 }
 
 void log_token_list(const t_token_list *tkl)
