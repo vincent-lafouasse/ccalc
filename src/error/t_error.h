26754b5f75cf2bc0254e6050d8a2b505f58058c9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-26 13:17:27 +0200

    import log and error module from minishell

---
+++ src/error/t_error.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef enum e_error
+{
+    NO_ERROR,
+    E_UNRECOGNIZED_TOKEN,
+    E_UNTERMINATED_QUOTE,
+    E_OOM,
+    E_UNREACHABLE,
+    E_NULL_PARAM,
+    E_DUMMY,
+} t_error;
+
+const char *error_repr(t_error err);
+void die(const char *msg);
+void ft_assert(bool assertion, const char *msg);
