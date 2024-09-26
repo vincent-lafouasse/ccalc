8f273a55198b93240cf78b170a42e5f083ce53d8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-26 15:27:23 +0200

    reinstate basic interpreter

--- src/ccalc.c
+++ src/ccalc.c
@@ -7,6 +7,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include "readline/readline.h"
+#include "readline/history.h"
+
 t_token Token(t_token_type ty)
 {
 	return (t_token){.type = ty};
@@ -55,9 +62,11 @@ t_token_list* tokenize(const char* in)
 		in++;
 	}
 
+	tkl_push_back(&out, Token(EOF_TOKEN));
 	return out;
 }
 
+#if 0
 int main(void)
 {
 	t_token_list* tokens = NULL;
@@ -77,4 +86,38 @@ int main(void)
 	else
 		printf("rip bozo\n");
 
+}
+#endif
+
+static bool string_empty(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!isspace(str[i]))
+			return (false);
+	}
+	return (true);
+}
+
+int main(void)
+{
+	char *line;
+	t_token_list* tokens;
+
+	while ((line = readline("$ ")))
+	{
+		printf("%s\n", line);
+		if (!string_empty(line))
+			add_history(line);
+		tokens = tokenize(line);
+		if (parse(tokens))
+			printf("ok\n");
+		else
+			printf("rip bozo\n");
+		free(line);
+		tkl_clear(&tokens);
+	}
 }
