e5c274b28793421ce6bc02b702f808ca5692452d
bitquence <imbitquence@gmail.com>
2024-09-26 13:20:31 +0200

    add simple readline and print loop

---
+++ src/ccalc.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include "readline/readline.h"
+#include "readline/history.h"
+
+static bool string_empty(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!isspace(str[i]))
+			return (false);
+	}
+	return (true);
+}
+
+int main(void)
+{
+	char *line;
+
+	while ((line = readline("$ ")))
+	{
+		printf("%s\n", line);
+		if (!string_empty(line))
+			add_history(line);
+		free(line);
+	}
+}
