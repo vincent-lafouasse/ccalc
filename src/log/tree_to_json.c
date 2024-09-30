6e231b31433a5336ee21bf63d9e80a64002520a9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-30 18:51:55 +0200

    basic node type repr

--- src/log/tree_to_json.c
+++ src/log/tree_to_json.c
@@ -2,6 +2,28 @@
 
 #include <stdio.h>
 
+const char* node_repr(enum e_symbol_kind kind)
+{
+
+    if (kind == TERMINAL)
+        return "Terminal";
+    if (kind == EXPR)
+        return "Expr";
+    if (kind == EXPR_REST)
+        return "Expr'";
+    if (kind == TERM)
+        return "Term";
+    if (kind == TERM_REST)
+        return "Term'";
+    if (kind == FACTOR)
+        return "Factor";
+    return "???";
+}
+
+void print_node(const t_symbol* node)
+{
+}
+
 void tree_to_json(const t_symbol *root)
 {
     (void)root;
@@ -9,7 +31,9 @@ void tree_to_json(const t_symbol *root)
 
     printf("chart: { container: \"#tree-simple\" },");
 
-    printf("nodeStructure: { text: {name: \"root\"} }");
+    printf("nodeStructure: ");
 
-    printf("};");
+    print_node(root);
+
+    printf("};\n");
 }
